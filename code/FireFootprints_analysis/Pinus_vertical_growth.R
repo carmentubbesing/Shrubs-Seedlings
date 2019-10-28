768ae6de242de1be23928aa3dc93372bda56ad3e
Carmen Tubbesing <ctubbesing@berkeley.edu>
2019-10-28 13:58:32 -0700

    simulations init commit

--- code/FireFootprints_analysis/Pinus_vertical_growth.R
+++ code/FireFootprints_analysis/Pinus_vertical_growth.R
@@ -1,14 +1,4 @@
----
-title: 'Pinus with shrubarea vertical analysis - fire footprints'
-author: "Carmen"
-date: "March 9 + 2017"
-output: 
-  html_document:
-    toc: true
-    toc_depth: 2
----
-
-```{r, include = F}
+## ---- include = F--------------------------------------------------------
 require(nlme)
 require(randomForest)
 require(tree)
@@ -20,51 +10,48 @@ require(effects)
 require(MuMIn)
 require(car)
 set.seed(12346)
-```
 
-# Load data
-```{r}
+
+## ------------------------------------------------------------------------
 setwd("~/Shrubs-Seedlings/code/FireFootprints_analysis/")
 load(file="../../compiled_data/fire_footprints/master_seedlings_vert.Rdata")
-```
 
-# Load VSURF results
-```{r}
+
+## ------------------------------------------------------------------------
 load("../../results/VSURF_pipo_vars.Rdata")
-```
 
-# Before filtering to just PIPO save min and max shrub cover for growth estiamtes at the end. Use min and max that have BOTH SPECIES
-```{r}
+
+## ------------------------------------------------------------------------
 df_summary <- df %>% 
   group_by(Species) %>% 
   summarize(min= min(sqrt_shrubarea3), max = max(sqrt_shrubarea3), median = median(sqrt_shrubarea3))
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 min_shrub <- df_summary %>% 
   filter(min == max(min)) %>% 
   select(min) %>% 
   unlist()
 min_shrub
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 max_shrub <- df_summary %>% 
   filter(max == min(max)) %>% 
   select(max) %>% 
   unlist()
 max_shrub
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 median_shrub <- df %>% 
   mutate(shrubarea3 = sqrt(shrubarea3)) %>%
   summarize(median = median(shrubarea3)) %>% 
   unlist
 median_shrub
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 summary <- summary(sqrt(df$shrubarea3))
 summary
 quart1 <- summary[2]
@@ -72,10 +59,9 @@ quart3 <- summary[5]
 mean <- mean(sqrt(df$shrubarea3))
 shrubarea_range_conservative <- cbind(min, max, median)
 shrubarea_range_conservative
-```
 
-# Filter to just PIPO
-```{r}
+
+## ------------------------------------------------------------------------
 df %>% 
   filter(Species == "PIPO") %>% 
   group_by(Fire) %>% 
@@ -83,15 +69,14 @@ df %>%
   arrange(`n()`)
 df <- df %>% 
   filter(Species == "PIPO")
-```
 
-# Count totals and by fire
-```{r}
+
+## ------------------------------------------------------------------------
 length(unique(df$Sdlg))
 nrow(df)
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 df %>% 
   group_by(Sdlg, Fire) %>%
   select(Sdlg, Fire) %>%
@@ -100,28 +85,23 @@ df %>%
   group_by(Fire) %>% 
   summarize(n = n()) %>% 
   mutate(total = sum(n))
-```
 
-# Find average growth rate
-```{r}
+
+## ------------------------------------------------------------------------
 mean(df$VertGrowth_Rel)
 sd(df$VertGrowth_Rel)
 sd(df$VertGrowth_Rel)/sqrt(nrow(df))
 ggplot(df)+
   geom_boxplot(aes(y = VertGrowth_Rel))
-```
 
-# Structure data for LM
 
-## Normalize function
-```{r}
+## ------------------------------------------------------------------------
 normalize <- function(x) {
     return ((x - mean(x)) / sd(x))
   }
-```
 
-## filter and normalize df
-```{r}
+
+## ------------------------------------------------------------------------
 LMdf <- df %>% 
   mutate(sqrt_shrubarea3 = sqrt(shrubarea3)) %>%
   mutate(log_shrubarea3 = log(shrubarea3+1)) %>%
@@ -142,60 +122,33 @@ LMdf <- df %>%
  mutate_if(is.numeric, normalize) %>% 
   mutate(VertGrowth_Rel = df$VertGrowth_Rel)
 LMdf <- droplevels(LMdf)
-```
 
-# Model using VSURF var results
 
-## Convert vars to formula structure
-```{r}
+## ------------------------------------------------------------------------
 vars_one <- paste(vars, collapse = " + ")
 vars_one
 f <- formula(paste("VertGrowth_Rel ~ ",vars_one))
 f
-```
 
-## Run lme model
-```{r}
+
+## ------------------------------------------------------------------------
 LM <- lme(f, data = LMdf, random = ~ 1| Fire/Sdlg, method = "ML")
-```
 
-# Check collinearity
 
-## Make collinearity plot using Zuur code
-```{r}
+## ------------------------------------------------------------------------
 source("~/../Documents/HighstatLibV10.R.txt")
 z <- cbind(df$VertGrowth_Rel, df$Years, df$heatload, df$Ht_cm1, df$BasDia2016.cm, df$sqrt_shrubarea3, df$Elevation, df$Year)
 colnames(z) <- c("Growth", "Years", "heatload", "Ht_cm1", "BasDia", "shrubs", "Elevation", "Year")
 pairs(z, lower.panel = panel.smooth2, upper.panel = panel.cor, diag.panel = panel.hist)
-```
 
 
-## Calculate vif function from car package
-```{r}
+## ------------------------------------------------------------------------
 source("~/../Documents/HighstatLibV10.R.txt") # from Zuur https://highstat.com/index.php/mixed-effects-models-and-extensions-in-ecology-with-r
 corvif(LMdf %>% select(!!vars))
 vif(LM)
-```
 
-<!-- A VIF cutoff value of 5 is often used, and none of my variables have values larger than that, so I think I'm ok. -->
 
-<!-- ## Take out variable with the highest VIF -->
-<!-- ```{r} -->
-<!-- f <-  update(as.formula(f), ~ . - Ht_cm1) -->
-<!-- f -->
-<!-- LM <- lme(f, data = LMdf, random = ~ 1| Fire/Sdlg, method = "ML") -->
-<!-- vif(LM) -->
-<!-- ``` -->
-
-<!-- # Repeat until VIF < 5 -->
-<!-- ```{r} -->
-<!-- f <- update(as.formula(f), ~ . - Years)  -->
-<!-- LM <- lme(f, data = LMdf, random = ~ 1| Fire/Sdlg, method = "ML") -->
-<!-- vif(LM) -->
-<!-- ``` -->
-
-# See whether model with or without DIA interaction is better
-```{r}
+## ------------------------------------------------------------------------
 f_int <- update(as.formula(f), ~ . + BasDia2016.cm:sqrt_shrubarea3)
 LM_int <- lme(f_int, data = LMdf, random = ~ 1| Fire/Sdlg, method = "ML")
 AICcmodavg::AICc(LM)
@@ -203,10 +156,9 @@ AICcmodavg::AICc(LM_int)
 if(
   AICcmodavg::AICc(LM) > AICcmodavg::AICc(LM_int)
 ) print(paste("WITH INTERACTION WINS BY", round(AICcmodavg::AICc(LM)-AICcmodavg::AICc(LM_int), digits = 2), "AIC")) else print(paste("WITHOUT INTERACTION WINS BY", round(AICcmodavg::AICc(LM_int)-AICcmodavg::AICc(LM), digits = 2)))
-```
 
-# See whether model with or without HEIGHT interaction is better
-```{r}
+
+## ------------------------------------------------------------------------
 vars_int <- paste(vars_one, "+ Ht_cm1:sqrt_shrubarea3")
 f_int <- formula(paste("VertGrowth_Rel ~ ",vars_int))
 LM_int <- lme(f_int, data = LMdf, random = ~ 1| Fire/Sdlg, method = "ML")
@@ -215,47 +167,39 @@ AICcmodavg::AICc(LM_int)
 if(
   AICcmodavg::AICc(LM) > AICcmodavg::AICc(LM_int)
 ) print(paste("WITH INTERACTION WINS BY", round(AICcmodavg::AICc(LM)-AICcmodavg::AICc(LM_int), digits = 2), "AIC")) else print(paste("WITHOUT INTERACTION WINS BY", round(AICcmodavg::AICc(LM_int)-AICcmodavg::AICc(LM), digits = 2)))
-```
 
-# See whether residuals are better for log transformed or not
-```{r}
+
+## ------------------------------------------------------------------------
 plot(LM)
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 f_no_log <- formula(paste("exp(VertGrowth_Rel) ~", vars_one))
 LM_no_log <-  lme(f_no_log, data = LMdf, random = ~ 1| Fire/Sdlg, method = "ML")
 plot(LM_no_log)
-```
 
-Log transformed looks better.
 
-# Look at summary and drop1 results
-```{r}
+## ------------------------------------------------------------------------
 summary(LM)
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 drop <- drop1(LM, test = "Chisq")
 drop$var <- row.names(drop)
 drop <- tbl_df(drop)
 drop %>% 
   arrange( `Pr(>Chi)`) %>% 
   mutate( `Pr(>Chi)` = paste( `Pr(>Chi)`))
-```
 
-# Look at collinearity between seedling height and shrub competition
-```{r}
+
+## ------------------------------------------------------------------------
 cor(df$Ht_cm1, df$sqrt_shrubarea3)
 ggplot(df, aes(x = Ht_cm1, y = sqrt_shrubarea3))+
   geom_point()+
   geom_smooth(method = "lm")
-```
 
-# Look at residuals
 
-## Residuals
-```{r}
+## ------------------------------------------------------------------------
 E <- resid(LM, type = "normalized")
 df$E <- E
 pred <- predict(LM)
@@ -270,49 +214,38 @@ plot(df$Year, E)
 ggplot(df, aes(x = log(df$shrubarea3), y = E))+
   geom_point()+
   geom_smooth()
-```
 
-# Look at individual effects
 
-Note: the plots look exactly the same but with different axes if you don't normalize the numeric predictors. So for including plots to visualize in the paper, I just use non-normalized data
-
-```{r}
+## ------------------------------------------------------------------------
 plot(predictorEffect("Years", LM))
 plot(predictorEffect("sqrt_shrubarea3", LM))
 plot(predictorEffect("Ht_cm1", LM))
 plot(predictorEffect("ShrubSpp03", LM))
-```
 
-# Calculate Nakagawa's pseudo R2
-```{r}
+
+## ------------------------------------------------------------------------
 r.squaredGLMM(LM)
-```
 
-# Plot shrub effects without interaction
 
-## Re-create effects plots using predictorEffects() and ggplot
-
-```{r}
+## ------------------------------------------------------------------------
 eff_shrubarea <- predictorEffect("sqrt_shrubarea3", LM)
 effects_df <- as.data.frame(eff_shrubarea)
 head(effects_df)
-```
 
-## Transform variables back to original units
-```{r}
+
+## ------------------------------------------------------------------------
 effects_df <- effects_df %>% 
   mutate(sqrt_shrubarea3 = sqrt_shrubarea3*sd(sqrt(df$shrubarea3))+mean(sqrt(df$shrubarea3)) ) 
-```
 
-## Save min and max bounds for use in the fir graph
-```{r}
+
+## ------------------------------------------------------------------------
 plot_min <- exp(min(effects_df$lower))
 plot_max <- exp(max(effects_df$upper))
 save(plot_min, file = "../../results/figures/FireFootprints/plot_min.Rdata")
 save(plot_max, file = "../../results/figures/FireFootprints/plot_max.Rdata")
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 ggplot(effects_df)+
   geom_line(aes(x = sqrt_shrubarea3, y = exp(fit)), col = "#fc8d62")+
   geom_ribbon(aes(x = sqrt_shrubarea3, ymin = exp(lower), ymax = exp(upper)), fill = "#fc8d62",   alpha = .4)+
@@ -329,40 +262,30 @@ ggplot(effects_df)+
         panel.grid.major.x = element_blank(),
         text = element_text(size = 11))
 ggsave(file = "../../results/figures/FireFootprints/PineVertRel_noint.png", width = 3, height = 3, dpi = 400)
-```
 
 
-# Look at relationship between years and shrubarea
-```{r}
+## ------------------------------------------------------------------------
 ggplot(df)+
   geom_boxplot(aes(x = Fire, y = shrubarea3))
 ggplot(df)+
   geom_boxplot(aes(x = as.factor(Years), y = shrubarea3))
-```
 
-# Summarize range of shrub cover and height
-```{r}
+
+## ------------------------------------------------------------------------
 summary(df$Cov1.3)
 summary(df$Ht1.3)
 summary(df$shrubarea3)
-```
 
-# Convert cover to Percent
-```{r}
+
+## ------------------------------------------------------------------------
 summary(df$Cov1.3/(300*4))
-```
 
-# Save LM
-```{r}
+
+## ------------------------------------------------------------------------
 save(LM, file = "../../results/data/FireFootprints/LM_pipo.Rdata")
-```
 
 
-
-# Find predicted mean growth for min, max, median
-
-## min
-```{r}
+## ------------------------------------------------------------------------
 newdata_means <- as.data.frame(LMdf) %>% 
   ungroup() %>% 
   summarise_if(is.numeric, .funs = c("mean")) %>% 
@@ -379,11 +302,9 @@ Fires <- LMdf %>% select(Sdlg, Fire) %>% mutate(Fire = as.factor(Fire))
 newdata <- full_join(Fires, newdata)
 pred <- predict(LM, newdata = newdata)
 gr_minshrub_pipo <- exp(mean(pred))
-```
 
 
-## max
-```{r}
+## ------------------------------------------------------------------------
 newdata_means <- as.data.frame(LMdf) %>% 
   ungroup() %>% 
   summarise_if(is.numeric, .funs = c("mean")) %>% 
@@ -400,11 +321,9 @@ Fires <- LMdf %>% select(Sdlg, Fire) %>% mutate(Fire = as.factor(Fire))
 newdata <- full_join(Fires, newdata)
 pred <- predict(LM, newdata = newdata)
 gr_maxshrub_pipo <- exp(mean(pred))
-```
 
 
-## median
-```{r}
+## ------------------------------------------------------------------------
 newdata_means <- as.data.frame(LMdf) %>% 
   ungroup() %>% 
   summarise_if(is.numeric, .funs = c("mean")) %>% 
@@ -421,21 +340,15 @@ Fires <- LMdf %>% select(Sdlg, Fire) %>% mutate(Fire = as.factor(Fire))
 newdata <- full_join(Fires, newdata)
 pred <- predict(LM, newdata = newdata)
 gr_medshrub_pipo <- exp(mean(pred))
-```
 
 
-## Save them
-```{r}
+## ------------------------------------------------------------------------
 save(gr_minshrub_pipo, file = "../../results/data/FireFootprints/gr_minshrub_pipo.Rdata")
 save(gr_maxshrub_pipo, file = "../../results/data/FireFootprints/gr_maxshrub_pipo.Rdata")
 save(gr_medshrub_pipo, file = "../../results/data/FireFootprints/gr_medshrub_pipo.Rdata")
-```
 
 
-
-## Similar method but using results from effects package
-
-```{r}
+## ------------------------------------------------------------------------
 fit_median <- effects_df[which.min(abs(effects_df$sqrt_shrubarea3 - median_shrub)),]$fit
 fit_median
 fit_min <- effects_df[which.min(abs(effects_df$sqrt_shrubarea3 - min_shrub)),]$fit
@@ -450,32 +363,30 @@ print(paste("fit_median_log = ", fit_median))
 print(paste("fit_min_log = ", fit_min))
 print(paste("fit_max_log = ", fit_max))
 #print(paste("fit_mean_log = ", fit_mean))
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 gr_minshrub_pipo <- exp(fit_min)
 gr_maxshrub_pipo <- exp(fit_max)
 gr_medshrub_pipo <- exp(fit_median)
-```
 
-## Repeat for 60% shrub cover and 86 cm ht similar to what's in COllins and ROller
-```{r}
+
+## ------------------------------------------------------------------------
 collins_shrubarea <- sqrt((.6*1200)*86)
 effects_df[which.min(abs(effects_df$sqrt_shrubarea3 - quart1)),]$fit
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 sd <- sd(df$shrubarea3/10000)
-```
 
-# Pretty plot of shrub species effects
-```{r}
+
+## ------------------------------------------------------------------------
 eff_shrubspp <- predictorEffect("ShrubSpp03", LM)
 effects_df <- as.data.frame(eff_shrubspp)
 head(effects_df)
-```
 
-```{r}
+
+## ------------------------------------------------------------------------
 ggplot(effects_df %>% filter(ShrubSpp03 != "Other"  ))+
   geom_point(aes(x = ShrubSpp03, y = exp(fit)))+
   ylim(.1, .25)+
@@ -500,22 +411,19 @@ ggplot(effects_df %>% filter(ShrubSpp03 != "Other"  ))+
 
 
 ggsave(file = "../../results/figures/FireFootprints/PineVertShrubSpp.png", width = 4, height = 4, dpi = 400)
-```
 
-# List the final seedlings used
-```{r}
+
+## ------------------------------------------------------------------------
 sort(unique(df$Sdlg))
-```
 
-# Count how many trees have estimated 2015 heights
-```{r}
+
+## ------------------------------------------------------------------------
 df %>% 
   filter(Year==2015) %>% 
   filter(is.na(Ht2017.cm.fall) & is.na(Ht2015.meas2016)) %>% 
   nrow()
-```
 
-# Save final data
-```{r}
+
+## ------------------------------------------------------------------------
 save(df, file ="../../compiled_data/fire_footprints/pine_vert.Rdata")
-```
+
