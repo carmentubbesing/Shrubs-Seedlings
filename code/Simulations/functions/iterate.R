ced22b820162e6de43cf1c579247dc6779f8233b
Carmen Tubbesing <ctubbesing@berkeley.edu>
2020-01-22 17:42:15 -0800

    predict dia growth based on dendro data

--- code/Simulations/functions/iterate.R
+++ code/Simulations/functions/iterate.R
@@ -1,7 +1,11 @@
 dfsimallreps <- data.frame()
-
+time.start <- Sys.time()
 iterate <- function(n){
   for(i in 1:n){
+    i_tenth <- i/10
+    if(i_tenth %in% seq(1,100)){
+      print(paste("Done with", i, "iterations in", round(Sys.time()-time.start, 1), "minutes"))
+    }
     suppressMessages(initialize())
     suppressMessages(sim(years))
     
