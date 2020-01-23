e6294f31fa9d285119eaff82188a66a075fcfdd0
Carmen Tubbesing <ctubbesing@berkeley.edu>
2020-01-23 15:22:32 -0800

    add dead trees to dendro dia~gr models

--- code/Simulations/functions/abcodia.R
+++ code/Simulations/functions/abcodia.R
@@ -1,15 +1,16 @@
 abcodia <- function(){
   
-  load("../../results/coefficients/LM_dia_abco.Rdata")
-  coefabco <<- lmabco$coefficients
+  load("../../results/coefficients/LM_dia_abco_dendro.Rdata")
+  coefabco <<- lmeabco$coefficients$fixed
   
   pts.sf.abco <<- pts.sf.abco %>% 
-    mutate(dia.cm = coefabco["(Intercept)"] +
-             coefabco["height_2018"]*Ht_cm1+
-             coefabco["sqrt(shrubarea)"]*sqrt_shrubarea3+
-             coefabco["dia17_cm_Aug"]*dia.cm+
-             coefabco["height_2018:dia17_cm_Aug"]*Ht_cm1*dia.cm) 
-  # ggplot(pts.sf.abco, aes(x = BasDia2016.cm, y = dia.cm))+
-  #   geom_point()+
-  #   geom_abline(aes(slope = 1, intercept = 0))
+    mutate(dia.growth.rel.log = coefabco["(Intercept)"] +
+             coefabco["pre_growth_height"]*Ht_cm1+
+             coefabco["pre_growth_diameter"]*dia.cm+
+             coefabco["ht_growth"]*pred) %>% 
+    mutate(dia.growth.rel = exp(dia.growth.rel.log)) %>% 
+    mutate(dia.cm = dia.cm + dia.growth.rel*dia.cm)   # calculate new ht after growth
+   ggplot(pts.sf.abco, aes(x = dia.cm, y = dia.cm))+
+     geom_point()+
+     geom_abline(aes(slope = 1, intercept = 0))
 }
--- code/Simulations/functions/pipodia.R
+++ code/Simulations/functions/pipodia.R
@@ -1,15 +1,17 @@
 pipodia <- function(){
   
-  load("../../results/coefficients/LM_dia_pipo.Rdata")
-  coefpipo <<- lmpipo$coefficients
+  
+  load("../../results/coefficients/LM_dia_pipo_dendro.Rdata")
+  coefpipo <<- lmepipo$coefficients$fixed
   
   pts.sf.pipo <<- pts.sf.pipo %>% 
-    mutate(dia.cm = coefpipo["(Intercept)"] +
-             coefpipo["height_2018"]*Ht_cm1+
-             coefpipo["sqrt(shrubarea)"]*sqrt_shrubarea3+
-             coefpipo["dia17_cm_Aug"]*dia.cm+
-             coefpipo["height_2018:dia17_cm_Aug"]*Ht_cm1*dia.cm) 
-  # ggplot(pts.sf.pipo, aes(x = BasDia2016.cm, y = dia.cm))+
-  #   geom_point()+
-  #   geom_abline(aes(slope = 1, intercept = 0))
+    mutate(dia.growth.rel.log = coefpipo["(Intercept)"] +
+             coefpipo["pre_growth_height"]*Ht_cm1+
+             coefpipo["pre_growth_diameter"]*dia.cm+
+             coefpipo["ht_growth"]*pred) %>% 
+    mutate(dia.growth.rel = exp(dia.growth.rel.log)) %>% 
+    mutate(dia.cm = dia.cm + dia.growth.rel*dia.cm)   # calculate new ht after growth
+  ggplot(pts.sf.pipo, aes(x = dia.cm, y = dia.cm))+
+    geom_point()+
+    geom_abline(aes(slope = 1, intercept = 0))
 }
