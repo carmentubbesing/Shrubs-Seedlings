9947143a82f1250deb23fb4e467debb18355ce9c
Carmen Tubbesing <ctubbesing@berkeley.edu>
2020-01-30 11:59:34 -0800

    add shrub cov growth function

--- code/Simulations/functions/abco_shrubgrowth.R
+++ code/Simulations/functions/abco_shrubgrowth.R
@@ -8,22 +8,22 @@ abco_shrubgrowth <- function(){
   load("~/Shrubs-Seedlings/results/coefficients/gamOTHER.Rdata")
   
   # Add a column for mean predicted shrub height for the present year, predicted by the GAM for each species
-  x <- data.frame(years_since_fire = unique(pts.sf.abco$Years))
+  x1 <- data.frame(years_since_fire = unique(pts.sf.abco$Years))
 
   pts.sf.abco <<- pts.sf.abco %>% 
     mutate(mean_shrub_ht_by_spp = case_when(
-      ShrubSpp03 == "CECO" ~ predict.gam(gamCECO, x, se= T)$fit,
-      ShrubSpp03 == "ARPA" ~ predict.gam(gamARPA, x, se= T)$fit,
-      ShrubSpp03 == "CEIN" ~ predict.gam(gamCEIN, x, se= T)$fit,
-      ShrubSpp03 == "CHSE" ~ predict.gam(gamCHSE, x, se= T)$fit,
-      TRUE  ~ predict.gam(gamOTHER, x, se= T)$fit
+      ShrubSpp03 == "CECO" ~ predict.gam(gamCECO, x1, se= T)$fit,
+      ShrubSpp03 == "ARPA" ~ predict.gam(gamARPA, x1, se= T)$fit,
+      ShrubSpp03 == "CEIN" ~ predict.gam(gamCEIN, x1, se= T)$fit,
+      ShrubSpp03 == "CHSE" ~ predict.gam(gamCHSE, x1, se= T)$fit,
+      TRUE  ~ predict.gam(gamOTHER, x1, se= T)$fit
     )) %>% 
     mutate(se_shrub_ht_by_spp = case_when(
-      ShrubSpp03 == "CECO" ~ predict.gam(gamCECO, x, se= T)$se.fit,
-      ShrubSpp03 == "ARPA" ~ predict.gam(gamARPA, x, se= T)$se.fit,
-      ShrubSpp03 == "CEIN" ~ predict.gam(gamCEIN, x, se= T)$se.fit,
-      ShrubSpp03 == "CHSE" ~ predict.gam(gamCHSE, x, se= T)$se.fit,
-      TRUE  ~ predict.gam(gamOTHER, x, se= T)$se.fit
+      ShrubSpp03 == "CECO" ~ predict.gam(gamCECO, x1, se= T)$se.fit,
+      ShrubSpp03 == "ARPA" ~ predict.gam(gamARPA, x1, se= T)$se.fit,
+      ShrubSpp03 == "CEIN" ~ predict.gam(gamCEIN, x1, se= T)$se.fit,
+      ShrubSpp03 == "CHSE" ~ predict.gam(gamCHSE, x1, se= T)$se.fit,
+      TRUE  ~ predict.gam(gamOTHER, x1, se= T)$se.fit
     ))
     
   # Check
@@ -35,22 +35,22 @@ abco_shrubgrowth <- function(){
     mutate(diff_shr_ht = Ht1.3 - mean_shrub_ht_by_spp)
   
   # Repeat the mean height calculations for the next year
-  x <- data.frame(years_since_fire = unique(pts.sf.abco$Years+1))
+  x2 <- data.frame(years_since_fire = unique(pts.sf.abco$Years+1))
   
   pts.sf.abco <<- pts.sf.abco %>% 
     mutate(mean_shrub_ht_by_spp_T2 = case_when(
-      ShrubSpp03 == "CECO" ~ predict.gam(gamCECO, x, se= T)$fit,
-      ShrubSpp03 == "ARPA" ~ predict.gam(gamARPA, x, se= T)$fit,
-      ShrubSpp03 == "CEIN" ~ predict.gam(gamCEIN, x, se= T)$fit,
-      ShrubSpp03 == "CHSE" ~ predict.gam(gamCHSE, x, se= T)$fit,
-      TRUE  ~ predict.gam(gamOTHER, x, se= T)$fit
+      ShrubSpp03 == "CECO" ~ predict.gam(gamCECO, x2, se= T)$fit,
+      ShrubSpp03 == "ARPA" ~ predict.gam(gamARPA, x2, se= T)$fit,
+      ShrubSpp03 == "CEIN" ~ predict.gam(gamCEIN, x2, se= T)$fit,
+      ShrubSpp03 == "CHSE" ~ predict.gam(gamCHSE, x2, se= T)$fit,
+      TRUE  ~ predict.gam(gamOTHER, x2, se= T)$fit
     )) %>% 
     mutate(se_shrub_ht_by_spp_T2 = case_when(
-      ShrubSpp03 == "CECO" ~ predict.gam(gamCECO, x, se= T)$se.fit,
-      ShrubSpp03 == "ARPA" ~ predict.gam(gamARPA, x, se= T)$se.fit,
-      ShrubSpp03 == "CEIN" ~ predict.gam(gamCEIN, x, se= T)$se.fit,
-      ShrubSpp03 == "CHSE" ~ predict.gam(gamCHSE, x, se= T)$se.fit,
-      TRUE  ~ predict.gam(gamOTHER, x, se= T)$se.fit
+      ShrubSpp03 == "CECO" ~ predict.gam(gamCECO, x2, se= T)$se.fit,
+      ShrubSpp03 == "ARPA" ~ predict.gam(gamARPA, x2, se= T)$se.fit,
+      ShrubSpp03 == "CEIN" ~ predict.gam(gamCEIN, x2, se= T)$se.fit,
+      ShrubSpp03 == "CHSE" ~ predict.gam(gamCHSE, x2, se= T)$se.fit,
+      TRUE  ~ predict.gam(gamOTHER, x2, se= T)$se.fit
     ))
   
   # Check
@@ -60,6 +60,35 @@ abco_shrubgrowth <- function(){
   # Now make height this new height plus the difference between predicted height and actual height for the present year
   pts.sf.abco <<- pts.sf.abco %>% 
     mutate(Ht1.3 = mean_shrub_ht_by_spp_T2 + diff_shr_ht)
+  
+  # Change shrub cover based on linear model from my data
+  load("~/Shrubs-Seedlings/results/coefficients/LM_shrubcover.Rdata")
+  
+  # Add a column for predicted shrub cover for the present year, predicted by the LM
+  pts.sf.abco <<- pts.sf.abco %>% 
+    mutate(predicted_shrub_cov = predict(lmALL.ME, newdata = pts.sf.abco)) %>% 
+    mutate(se_predicted_shrub_cov = predict(lmALL.ME, newdata = pts.sf.abco, se= T)$se.fit)
+  
+  # Add a column for difference between current cover and expected cover
+  pts.sf.abco <<- pts.sf.abco %>% 
+    mutate(diff_shr_cov = Cov1.3 - predicted_shrub_cov)
+  
+  x2 <- pts.sf.abco %>% 
+    mutate(Years = Years + 1)
+  
+  # Repeat the mean height calculations for the next year
+  pts.sf.abco <<- pts.sf.abco %>% 
+    mutate(predicted_shrub_cov_T2 = predict(lmALL.ME, newdata = x2)) %>% 
+    mutate(se_predicted_shrub_cov_T2 = predict(lmALL.ME, newdata = x2, se= T)$se.fit)
+  
+  # Check
+  ggplot(pts.sf.abco)+
+    geom_point(aes(x = predicted_shrub_cov, y = predicted_shrub_cov_T2))+
+    geom_abline(aes(intercept = 0, slope = 1))
+  
+  # Now make height this new height plus the difference between predicted height and actual height for the present year
+  pts.sf.abco <<- pts.sf.abco %>% 
+    mutate(Cov1.3 = predicted_shrub_cov_T2 + diff_shr_cov)
   
   # Re-calculate shrub indices
   pts.sf.abco <<- pts.sf.abco %>% 
--- code/Simulations/functions/pipo_shrubgrowth.R
+++ code/Simulations/functions/pipo_shrubgrowth.R
@@ -61,6 +61,37 @@ pipo_shrubgrowth <- function(){
   pts.sf.pipo <<- pts.sf.pipo %>% 
     mutate(Ht1.3 = mean_shrub_ht_by_spp_T2 + diff_shr_ht)
   
+  
+  # Change shrub cover based on linear model from my data
+  load("~/Shrubs-Seedlings/results/coefficients/LM_shrubcover.Rdata")
+  
+  # Add a column for predicted shrub cover for the present year, predicted by the LM
+  pts.sf.pipo <<- pts.sf.pipo %>% 
+    mutate(predicted_shrub_cov = predict(lmALL.ME, newdata = pts.sf.pipo)) %>% 
+    mutate(se_predicted_shrub_cov = predict(lmALL.ME, newdata = pts.sf.pipo, se= T)$se.fit)
+  
+  # Add a column for difference between current cover and expected cover
+  pts.sf.pipo <<- pts.sf.pipo %>% 
+    mutate(diff_shr_cov = Cov1.3 - predicted_shrub_cov)
+  
+  x2 <- pts.sf.pipo %>% 
+    mutate(Years = Years + 1)
+  
+  # Repeat the mean height calculations for the next year
+  pts.sf.pipo <<- pts.sf.pipo %>% 
+    mutate(predicted_shrub_cov_T2 = predict(lmALL.ME, newdata = x2)) %>% 
+    mutate(se_predicted_shrub_cov_T2 = predict(lmALL.ME, newdata = x2, se= T)$se.fit)
+  
+  # Check
+  ggplot(pts.sf.pipo)+
+    geom_point(aes(x = predicted_shrub_cov, y = predicted_shrub_cov_T2))+
+    geom_abline(aes(intercept = 0, slope = 1))
+  
+  # Now make height this new height plus the difference between predicted height and actual height for the present year
+  pts.sf.pipo <<- pts.sf.pipo %>% 
+    mutate(Cov1.3 = predicted_shrub_cov_T2 + diff_shr_cov)
+  
+  
   # Re-calculate shrub indices
   pts.sf.pipo <<- pts.sf.pipo %>% 
     mutate(shrubarea3 = Cov1.3*Ht1.3) %>% 
