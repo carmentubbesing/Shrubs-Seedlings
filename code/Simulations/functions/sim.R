35cd40f67313ae0cdb5cba89d11fc8796351771c
Carmen Tubbesing <ctubbesing@berkeley.edu>
2020-02-12 19:00:15 -0800

    fix: run sim through year_max but don't grow trees, then filter out static years

--- code/Simulations/functions/sim.R
+++ code/Simulations/functions/sim.R
@@ -4,18 +4,6 @@ sim <- function(years_max){
   i <- 1
   for(i in 1:years_max){
     
-    if(max(pts.sf.abco$Years)>10 & all(pts.sf.abco$emerged==1) & all(pts.sf.abco$emerged==1)){
-      break()
-    }
-    
-    random <<- runif(1,0,1)
-    
-    climate_year <<- case_when(
-      random < cumsum_2015 ~ 2015,
-      random > cumsum_2015 & random < cumsum_2016 ~ 2016,
-      random > cumsum_2016 ~ 2017
-    ) 
-    
     pts.sf.abco <<- pts.sf.abco %>% 
       mutate(emerged = ifelse(
         (Ht_cm1*0.75)>Ht1.3, 1, 0
@@ -26,6 +14,30 @@ sim <- function(years_max){
         (Ht_cm1*0.75)>Ht1.3, 1, 0
       )) 
     
+    if(max(pts.sf.abco$Years)>10 & all(pts.sf.abco$emerged==1) & all(pts.sf.pipo$emerged==1)){
+      pts.sf.abco <<- pts.sf.abco %>% 
+        mutate(Years = Years + 1)
+      pts.sf.pipo <<- pts.sf.pipo %>% 
+        mutate(Years = Years + 1)
+      dfsim <- full_join(st_drop_geometry(pts.sf.pipo), st_drop_geometry(pts.sf.abco))
+      
+      if(nrow(dfsimall) == 0){
+        dfsimall <- dfsim
+      } else{
+        dfsimall <- full_join(dfsim, dfsimall)
+      }
+    dfsimall <<- dfsimall
+      next()
+    }
+    
+    random <<- runif(1,0,1)
+    
+    climate_year <<- case_when(
+      random < cumsum_2015 ~ 2015,
+      random > cumsum_2015 & random < cumsum_2016 ~ 2016,
+      random > cumsum_2016 ~ 2017
+    ) 
+    
     if(any(pts.sf.abco$emerged==0) ){
       pts.sf.abco <<- pts.sf.abco %>% 
         mutate(Year = climate_year) 
@@ -33,6 +45,9 @@ sim <- function(years_max){
       abcomort()
       abcodia()
       abco_shrubgrowth()
+      pts.sf.abco <<- pts.sf.abco %>% 
+        mutate(Years = Years + 1)
+    } else{
       pts.sf.abco <<- pts.sf.abco %>% 
         mutate(Years = Years + 1)
     }
@@ -45,6 +60,9 @@ sim <- function(years_max){
       pipodia()
       pipo_shrubgrowth()
       
+      pts.sf.pipo <<- pts.sf.pipo %>% 
+        mutate(Years = Years + 1)
+    }else{
       pts.sf.pipo <<- pts.sf.pipo %>% 
         mutate(Years = Years + 1)
     }
