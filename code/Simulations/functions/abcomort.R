f65eeadae4eaa3c1a14ba80acdfb2cbd3f730437
Carmen Tubbesing <ctubbesing@berkeley.edu>
2020-02-13 13:39:41 -0800

    add uncertainty to abcomort and pipomort

--- code/Simulations/functions/abcomort.R
+++ code/Simulations/functions/abcomort.R
@@ -1,11 +1,15 @@
 abcomort <- function(){
-  load("../../results/coefficients/mean_coef_gr_abco.Rdata")
-  load("../../results/coefficients/mean_coef_int_abco.Rdata")
+  load("../../results/coefficients/gr_mort_all_coefficients_abco.Rdata")
+  random_row <- sample(1:nrow(all_coefficients),1)
+  coef <- all_coefficients[random_row,]
+  coef_int <- unlist(coef[2])
+  coef_gr <- unlist(coef[1])
+  
   pts.sf.abco <<- pts.sf.abco %>% 
     mutate(emerged = ifelse(
       (Ht_cm1*0.75)>Ht1.3, 1, 0
     )) %>% 
-    mutate(prob_mort = exp(mean_coef_int + mean_coef_gr*pred)/(1+exp(mean_coef_int + mean_coef_gr*pred))) %>% 
+    mutate(prob_mort = exp(coef_int + coef_gr*pred)/(1+exp(coef_int + coef_gr*pred))) %>% 
     mutate(rand = runif(nrow(pts.sf.abco), 0,1)) %>% 
     mutate(dead = ifelse(rand<prob_mort & emerged ==0, 1, 0)) %>% 
     filter(dead ==0)
--- code/Simulations/functions/pipomort.R
+++ code/Simulations/functions/pipomort.R
@@ -1,13 +1,17 @@
 pipomort <- function(){
-  load("../../results/coefficients/mean_coef_gr_pipo.Rdata") # these coefficients use log(growth) in the odel
-  load("../../results/coefficients/mean_coef_int_pipo.Rdata")
+  load("../../results/coefficients/gr_mort_all_coefficients_pipo.Rdata") # these coefficients use log(growth) in the odel
+  random_row <- sample(1:nrow(all_coefficients),1)
+  coef <- all_coefficients[random_row,]
+  coef_int <- unlist(coef[2])
+  coef_gr <- unlist(coef[1])
+  
   pts.sf.pipo <<- pts.sf.pipo %>% 
     mutate(emerged = ifelse(
       (Ht_cm1*0.75)>Ht1.3, 1, 0
     )) %>% 
-    mutate(prob_mort = exp(mean_coef_int + mean_coef_gr*pred)/(1+exp(mean_coef_int + mean_coef_gr*pred))) %>% 
+    mutate(prob_mort = exp(coef_int + coef_gr*pred)/(1+exp(coef_int + coef_gr*pred))) %>% 
     mutate(rand = runif(nrow(pts.sf.pipo), 0,1)) %>% 
     mutate(dead = ifelse(rand<prob_mort & emerged ==0, 1, 0)) %>% 
     filter(dead ==0)
-  
+
 }
