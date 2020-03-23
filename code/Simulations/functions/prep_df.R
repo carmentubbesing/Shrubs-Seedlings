00f40a5f614e80125d39e20d417425db31a43fb0
Carmen Tubbesing <ctubbesing@berkeley.edu>
2020-03-23 15:25:54 -0700

    revert to empirical densities for all shrub runs

--- code/Simulations/functions/prep_df.R
+++ code/Simulations/functions/prep_df.R
@@ -2,6 +2,7 @@ prep_df <- function(fire, conifer_species_method, shrub_method, n_seedlings){
   load(file="../../compiled_data/fire_footprints/master_seedlings_vert.Rdata")
   dffull <- df
   
+  # filter and clean df
   df <- df %>%
     dplyr::select(Sdlg, Species, Cov1.3, Ht1.3, ShrubSpp03, shrubarea3, BasDia2016.cm, Ht2016.cm_spring, heatload, incidrad, Slope.Deg, Elevation, Fire, Years, Year) %>%
     filter(Year==2016) %>%
@@ -11,6 +12,7 @@ prep_df <- function(fire, conifer_species_method, shrub_method, n_seedlings){
     distinct() %>%
     droplevels()
   
+  # Randomize where the trees are if conifer_species_method = random
   if(conifer_species_method == "random"){
     sample <- sample(nrow(df), nrow(df), replace = T)
     df <- df %>% 
@@ -19,17 +21,9 @@ prep_df <- function(fire, conifer_species_method, shrub_method, n_seedlings){
       mutate(Ht2016.cm_spring = unlist(df[sample, "Ht2016.cm_spring"]))
   }
   
-  # if(shrub_method == "CECO"){
-  #   df <- df %>% 
-  #     mutate(ShrubSpp03 = "CECO") %>% 
-  #     mutate(ShrubSpp03 = as.factor(ShrubSpp03))
-  # } else if(shrub_method == "ARPA"){
-  #   df <- df %>% 
-  #     mutate(ShrubSpp03 = "ARPA") %>% 
-  #     mutate(ShrubSpp03 = as.factor(ShrubSpp03))
-  # }
+  # Randomly select seedlings with replacement
   
-  # randomly select seedlings with replacement
+  # If shrub_method = "welch", then select based on proportions in welch data
   if(shrub_method=="welch"){
     load("../../results/coefficients/welch_ratios.Rdata")
     welch_shrspp <- welch_shrspp %>% 
@@ -60,33 +54,19 @@ prep_df <- function(fire, conifer_species_method, shrub_method, n_seedlings){
     
   }
   
-  if(shrub_method %in% c("ARPA", "CECO")){
-    load("../../results/coefficients/welch_ratios.Rdata")
-    if(shrub_method == "ARPA") {
-      welch_shrspp <- welch_shrspp %>% 
-      filter(Shrub_species == "ARPA") %>% 
-      mutate(prop = sum/sum(sum))
-    } else if(shrub_method == "CECO"){
-      welch_shrspp <- welch_shrspp %>% 
-        filter(Shrub_species == "CECO") %>% 
-        mutate(prop = sum/sum(sum))
-    }
-    df_new <- data.frame()
-    for(i in 1:nrow(welch_shrspp)){
-      welch_i <- welch_shrspp[i,]
-      welch_prop_i <- welch_shrspp[i,"prop"] %>% unlist()
-      df_i <- df %>% filter(Species == welch_i$Species & ShrubSpp03 == welch_i$Shrub_species)
-      sample_i <- sample_n(df_i, size = n_seedlings*welch_prop_i, replace = T)
-      
-      if(nrow(sample_i)==0){
-        next()
-      } else if(nrow(df_new)==0){
-        df_new <- sample_i
-      } else{
-        df_new <- full_join(df_new, sample_i)    
-      }
-      
-    }
+  # If shrub_method is for a single shrub species, select only seedlings under one of those shrub species
+  if(shrub_method == "empirical"){
+    df_new <- sample_n(df, size = n_seedlings, replace = T)
+  } else if(shrub_method == "ARPA") {
+    df_new <- df %>% 
+      filter(ShrubSpp03 == "ARPA") %>% 
+      sample_n(size = n_seedlings, replace = T)
+  } else if(shrub_method == "CECO"){
+    df_new <- df %>% 
+      filter(ShrubSpp03 == "CECO") %>% 
+      sample_n(size = n_seedlings, replace = T)
+  } 
+  
     
     #check
     df_new %>%
@@ -94,10 +74,8 @@ prep_df <- function(fire, conifer_species_method, shrub_method, n_seedlings){
       count() %>%
       ungroup() %>%
       mutate(prop = n/sum(n))
-    welch_shrspp
-    df <- df_new
-  }
   
+    df <- df_new
   
   return(df)
 }
